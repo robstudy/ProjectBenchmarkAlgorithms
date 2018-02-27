ddc70e9ac20cbf5d0790a268fa25f1aa5e467400
Robert Garza <robstudent1988@gmail.com>
2018-02-27 11:49:29 -0800

    Created SelectionSort and BubbleSort template functions in SortingAlgorithms.h

--- SortingAlgorithms.h
+++ SortingAlgorithms.h
@@ -1,6 +1,6 @@
 //INSERTION SORT 
 template <typename T>
-void insertionSort(T *A, int size)
+void InsertionSort(T *A, int size)
 {
 	for (int j = 1; j < size; j++)
 	{
@@ -13,3 +13,44 @@ void insertionSort(T *A, int size)
 		A[i+1] = key;
 	}
 };
+
+//SELECTION SORT
+template <typename T>
+void SelectionSort(T *A, int size)
+{
+	int i, j, min_index, temp;
+	
+	//Move boundary of unsorted sub-array 
+	for(i = 0; i < size-1; i++)
+	{
+		//Find minimum element in unsorted array
+		min_index = i;
+		for(j = i+1; j < size; j++)
+			if(A[j] < A[min_index]) min_index = j;
+		
+		//Swap minimum element with the first element
+		temp = A[min_index];
+		A[min_index] = A[i];
+		A[i] = temp;
+	}
+};
+
+//BUBBLE SORT
+template <typename T>
+void BubbleSort(T *A, int size)
+{
+	int temp;
+	for(int i = 0; i < size; i++)
+	{
+		for(int j = 0; j < size-1; j++)
+		{
+			if(A[j] > A[j+1])
+			{
+				temp = A[j];
+				A[j] = A[j+1];
+				A[j+1] = temp;
+			}
+		}
+	}
+};
+				
--- main.cc
+++ main.cc
@@ -13,7 +13,31 @@ int main()
 	}
 	newArray.Display();
 	
-	insertionSort(newArray.GetArray(), newArray.GetSize());
+	InsertionSort(newArray.GetArray(), newArray.GetSize());
+	
+	newArray.Display();
+	
+	std::cout << "Selection Sort!\n";
+	for(int i = 0; i < 10; i++)
+	{
+		newArray.SetElement(i, rand()%100);
+	}
+	
+	newArray.Display();
+	
+	SelectionSort(newArray.GetArray(), newArray.GetSize());
+	
+	newArray.Display();
+	
+	std::cout << "Bubble Sort!\n";
+	for(int i = 0; i < 10; i++)
+	{
+		newArray.SetElement(i, rand()%100);
+	}
+	
+	newArray.Display();
+	
+	BubbleSort(newArray.GetArray(), newArray.GetSize());
 	
 	newArray.Display();
 	
