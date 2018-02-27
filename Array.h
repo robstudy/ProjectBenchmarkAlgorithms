5f60533595f0e5a2274f959e64cddefab3850520
Robert Garza <robstudent1988@gmail.com>
2018-02-27 10:45:17 -0800

    implemented Accessors and Modifiers in Array.h

--- Array.h
+++ Array.h
@@ -1,27 +1,139 @@
-#pragma once
-
 template <class T>
 class Array
 {
 	public:
 		//constructor
 		Array<T>();
-		Array(int n);
+		Array<T>(int);
 		//destructor
-		~Array();
+		~Array<T>();
 		
 		//Accessors + Modifiers
-		unsigned int GetSize();
-		int GetElement(int);
-		void SetElement(int, int);
-		bool Find(int);
-		void Delete(int);
-		void Insert(int);
-		T Search(int);
+		int GetSize();
+		T GetElement(int);
+		void SetElement(int, T);
+		bool Find(T);
+		void Delete(T);
+		void Insert(T);
+		int Search(T);
 		void Display();
 		
 	private:
 		T* A; //The array
-		unsigned int size; //the size
+		int size; //the size
 };
 		
+template <class T>
+Array<T>::Array()
+{
+	A = NULL;
+	size = 0;
+};
+
+template <class T>
+Array<T>::Array(int cSize)
+{
+	size = cSize;
+	A = new T[size];
+	for (int i = 0; i < size; i++) A[i] = 0;
+};
+
+template <class T>
+Array<T>::~Array()
+{
+	delete[] A;
+};
+
+template <class T>
+int Array<T>::GetSize() { return size; };
+
+template <class T>
+T Array<T>::GetElement(int index)
+{
+	if (index < 0) {
+		std::cout << "Index too small\n";
+		return 0;
+	} 
+	else if (index >= size) {
+		std::cout << "Index too large\n";
+		return 0;
+	} 
+	else
+		return A[index];
+};
+
+template <class T>
+void Array<T>::SetElement(int index, T data) 
+{
+	A[index] = data;
+};
+
+template <class T>
+bool Array<T>::Find(T data) 
+{
+	for(int i = 0; i < size; i++) 
+	{
+		if (A[i] == data) {
+			return true;
+		}
+	}
+	return false;
+};
+
+template <class T>
+void Array<T>::Delete(T data)
+{
+	if(Find(data))
+	{
+		T* temp = new T[size-1];
+		int j = 0;
+		for(int i = 0; i < size; i++) 
+		{
+			if(A[i] != data)
+			{
+				temp[j] = A[i];
+				j++;
+			}
+		}
+		delete[] A;
+		A = temp;
+	}
+};
+
+template <class T>
+void Array<T>::Insert(T data)
+{
+	T* temp = new T[size+1];
+	for(int i = 0; i < size; i++)
+	{
+		temp[i] = A[i];
+	}
+	temp[size] = data;
+	delete[] A;
+	A = temp;
+	size++;
+};
+
+template <class T>
+int Array<T>::Search(T data) 
+{
+	for (int i = 0; i < size; i++) 
+	{
+		if(A[i] == data)
+		{
+			return i;
+		}
+	}
+	return -1;
+};
+
+template <class T>
+void Array<T>::Display()
+{
+	for(int i = 0; i < size; i++) 
+	{
+		std::cout << A[i] << " ";
+	}
+	
+	std::cout << '\n';
+}
--- main.cc
+++ main.cc
@@ -4,7 +4,10 @@
 int main() 
 {
 	std::cout << "Hello World!\n";
+	Array<int> newArray(10);
 	
+	std::cout << newArray.GetSize() << std::endl;
+	newArray.Display();
 	
 	return 0;
 }
